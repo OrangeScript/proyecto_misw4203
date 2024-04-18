359d84d5f994e8b85ba933c8b656af3073130bf7
imdiegodev1 <da.naranjo10@uniandes.edu.co>
2024-04-17 21:01:26 -0500

    actividades aplicacion vinilos

---
+++ vinilos_app/app/src/main/java/com/example/vinilos/OpcionesActivity.kt
@@ -0,0 +1,20 @@
+package com.example.vinilos
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class OpcionesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_opciones)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
