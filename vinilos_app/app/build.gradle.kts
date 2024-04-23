cf3bb5fc677e37dc9df920f25d0d66a4610bb0c7
Juan Rodriguez <jp.rodriguez22@uniandes.edu.co>
2024-04-22 23:28:59 -0500

    Renderización de navbar en actividad principal

--- vinilos_app/app/build.gradle.kts
+++ vinilos_app/app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
--- vinilos_app/app/src/main/java/com/example/vinilos/MainActivity.kt
+++ vinilos_app/app/src/main/java/com/example/vinilos/MainActivity.kt
@@ -2,8 +2,30 @@ package com.example.vinilos
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.vinilos.ui.DetalleAlbum.DetalleAlbum
+import com.google.android.material.navigation.NavigationBarView
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navBar = findViewById<NavigationBarView>(R.id.nav_view)
+
+        navBar.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.fragment_detalle_album -> setCurrentFragment(DetalleAlbum())
+                R.id.fragment_lista_album -> setCurrentFragment(DetalleAlbum())
+            }
+            true
+        }
+    }
+
+    private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment_activity_main, fragment)
+            commit()
+        }
 }
