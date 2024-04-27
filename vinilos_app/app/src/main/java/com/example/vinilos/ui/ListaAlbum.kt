ad26e8b303f1c410e691b18978309321574c0128
Simón Buriticá <s.buritica@uniandes.edu.co>
2024-04-26 23:29:48 -0500

    Squashed commit of the following:
    commit 989f3595d632e48ee951805fe0c81cc0f9078bef
    Author: Simón Buriticá <s.buritica@uniandes.edu.co>
    Date:   Fri Apr 26 23:26:41 2024 -0500
    
        Borrar implementaciones inútiles
    
    commit eae04fee6f3ff661949026fde801f1de33accfb2
    Author: Simón Buriticá <s.buritica@uniandes.edu.co>
    Date:   Fri Apr 26 23:00:02 2024 -0500
    
        Se agrega estructura para el album adapter

--- vinilos_app/app/src/main/java/com/example/vinilos/ui/ListaAlbum.kt
+++ vinilos_app/app/src/main/java/com/example/vinilos/ui/ListaAlbum.kt
@@ -1,14 +1,19 @@
 package com.example.vinilos.ui
 
-import androidx.fragment.app.viewModels
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.vinilos.R
+import com.example.vinilos.databinding.FragmentListaAlbumBinding
+import com.example.vinilos.modelos.Album
+import com.example.vinilos.ui.adapters.AlbumAdapter
 import com.example.vinilos.viewmodels.ListaAlbumViewModel
 
 class ListaAlbum : Fragment() {
@@ -17,28 +22,61 @@ class ListaAlbum : Fragment() {
         fun newInstance() = ListaAlbum()
     }
 
-    private val viewModel: ListaAlbumViewModel by viewModels()
+    private var _binding: FragmentListaAlbumBinding? = null
+    private val binding get() = _binding!!
+
+
+    private lateinit var viewModel: ListaAlbumViewModel
 
     private lateinit var recyclerView: RecyclerView
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private var viewModelAdapter: AlbumAdapter? = null
 
-        // TODO: Use the ViewModel
-    }
+//    private lateinit var binding: FragmentListaAlbumBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val view = inflater.inflate(R.layout.fragment_lista_album, container, false)
-        //recyclerView = view.findViewById(R.id.rv_albums)
-        setupRecyclerView()
+        _binding = FragmentListaAlbumBinding.inflate(inflater, container, false)
+        val view = binding.root
+        viewModelAdapter = AlbumAdapter()
         return view
     }
 
-    private fun setupRecyclerView() {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recyclerView = binding.recyclerViewAlbums
         recyclerView.layoutManager = LinearLayoutManager(context)
-//        recyclerView.adapter = AlbumAdapter(viewModel)
+        recyclerView.adapter = viewModelAdapter
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val activity = requireNotNull(this.activity) {
+            "You can only access the viewModel after onActivityCreated()"
+        }
+        activity.actionBar?.title = getString(R.string.menu_albums)
+        viewModel = ViewModelProvider(this, ListaAlbumViewModel.Factory(activity.application))
+            .get(ListaAlbumViewModel::class.java)
+        viewModel.albums.observe(viewLifecycleOwner, Observer<List<Album>> {
+            it.apply {
+                viewModelAdapter!!.albums = this
+            }
+        })
+        viewModel.eventNetworkError.observe(viewLifecycleOwner, Observer<Boolean> { isNetworkError ->
+            if (isNetworkError) onNetworkError()
+        })
+    }
+
+    private fun onNetworkError() {
+        if (!viewModel.isNetworkErrorShown.value!!) {
+            Toast.makeText(activity, "Network Error", Toast.LENGTH_LONG).show()
+            viewModel.onNetworkErrorShown()
+        }
     }
 }
--- vinilos_app/app/src/main/java/com/example/vinilos/ui/adapters/AlbumAdapter.kt
+++ vinilos_app/app/src/main/java/com/example/vinilos/ui/adapters/AlbumAdapter.kt
@@ -1,4 +1,51 @@
 package com.example.vinilos.ui.adapters
 
-class AlbumAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.vinilos.R
+import com.example.vinilos.databinding.ItemAlbumBinding
+import com.example.vinilos.modelos.Album
+
+
+class AlbumAdapter: RecyclerView.Adapter<AlbumAdapter.AlbumViewHolder>() {
+
+    class AlbumViewHolder(val viewDataBinding: ItemAlbumBinding) : RecyclerView.ViewHolder(viewDataBinding.root) {
+        companion object {
+            @LayoutRes
+            val LAYOUT = R.layout.item_album
+        }
+    }
+
+    var albums: List<Album> = emptyList()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AlbumViewHolder {
+        val withDataBinding: ItemAlbumBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            AlbumViewHolder.LAYOUT,
+            parent,
+            false
+        )
+        return AlbumViewHolder(withDataBinding)
+    }
+
+    override fun onBindViewHolder(holder: AlbumViewHolder, position: Int) {
+        holder.viewDataBinding.also {
+            it.album = albums[position]
+        }
+      /*  holder.viewDataBinding.root.setOnClickListener {
+//            val action = ListaAlbumDirections.actionListaAlbuFragmentToAlbumFragment()
+            holder.viewDataBinding.root.findNavController().navigate(action)
+        }*/
+    }
+
+    override fun getItemCount(): Int {
+        return albums.size
+    }
 }
