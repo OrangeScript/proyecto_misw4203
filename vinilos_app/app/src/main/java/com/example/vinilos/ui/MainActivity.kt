a6878724e44fa66a29106013fb6344332a69867f
Juan Rodriguez <jp.rodriguez22@uniandes.edu.co>
2024-04-27 18:11:11 -0500

    Lista y detalle de album funcionando
    * Deja de funcionar el navbar para reconfigurar

--- vinilos_app/app/src/main/java/com/example/vinilos/ui/MainActivity.kt
+++ vinilos_app/app/src/main/java/com/example/vinilos/ui/MainActivity.kt
@@ -6,9 +6,11 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.vinilos.R
 import com.example.vinilos.databinding.ActivityMainBinding
 import com.example.vinilos.ui.Opciones.Opciones
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
@@ -18,32 +20,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        replaceFragment(ListaAlbum())
+//        replaceFragment(ListaAlbum())
 
         val navBar = findViewById<NavigationBarView>(R.id.nav_view)
-        val navHostFragment =supportFragmentManager
+        val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.fragment_activity_main) as NavHostFragment
 
         navController = navHostFragment.navController
-
-        binding.navView.setOnItemSelectedListener {
-            when (it.itemId) {
-                R.id.fragment_lista_artista -> replaceFragment(ListaBanda())
-                R.id.fragment_lista_album -> replaceFragment(ListaAlbum())
-                R.id.fragment_lista_coleccionita -> replaceFragment(ListaColeccionista())
-                R.id.fragment_opciones -> replaceFragment(Opciones())
-                else -> {
-
-                }
-            }
-            true
-        }
-    }
-    private fun replaceFragment(fragment: Fragment) {
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.fragment_activity_main, fragment)
-        fragmentTransaction.commit()
+        findViewById<BottomNavigationView>(R.id.nav_view)
+            .setupWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
