ca72c9a226f3953bced739911971aea8564c1f01
imdiegodev1 <da.naranjo10@uniandes.edu.co>
2024-04-28 14:59:00 -0500

    draft e2e test detalle album

--- vinilos_app/app/src/androidTest/java/com/example/vinilos/testDetalleAlbum.java
+++ vinilos_app/app/src/androidTest/java/com/example/vinilos/testDetalleAlbum.java
@@ -1,12 +1,14 @@
 package com.example.vinilos;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.filters.LargeTest;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+
 import com.example.vinilos.ui.MainActivity;
 
 import org.junit.Rule;
@@ -15,24 +17,21 @@ import org.junit.runner.RunWith;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class testListaAlbum {
+public class testDetalleAlbum {
 
     @Rule
     public ActivityScenarioRule<MainActivity> mActivityTestRule = new ActivityScenarioRule<>(MainActivity.class);
 
     @Test
-    public void mainActivityTest2() {
-        ViewInteraction listaAlbumExists = onView(withId(R.id.listaAlbum));
+    public void mainActivityTest3() {
+        ViewInteraction listaAlbumExiste = onView(withId(R.id.listaAlbum));
 
-        ViewInteraction elementoAlbumExiste = onView(withId(R.id.AlbumCardView));
+        ViewInteraction clickPrimerAlbum = onView(withId(R.id.AlbumCardView)).perform();
 
-        ViewInteraction coverAlbumExiste = onView(withId(R.id.albumCover));
+        //ViewInteraction clickPrimerElemento = onView(ViewMatchers.withId(R.id.listaAlbum)).perform(ViewActions.click());
 
-        ViewInteraction nombreAlbumExiste = onView(withId(R.id.albumName));
-
-        ViewInteraction generoAlbumExiste = onView(withId(R.id.albumGenre));
-
-        ViewInteraction fechaSalidaAlbumExiste = onView(withId(R.id.albumReleaseDate));
+        //ViewInteraction detalleAlbumExiste =
     }
-
 }
+
+
