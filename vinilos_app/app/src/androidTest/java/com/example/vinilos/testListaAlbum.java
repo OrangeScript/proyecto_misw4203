d42402a18cfcbce6fc4638712fade6e357e492cc
Sebastian Calderon <js.calderonb12@uniandes.edu.co>
2024-04-28 17:37:13 -0500

    Pruebas E2E detalle de lista (#88)
    * draft e2e test detalle album
    
    * Solución de conflictos
    
    ---------
    
    Co-authored-by: imdiegodev1 <da.naranjo10@uniandes.edu.co>
    Co-authored-by: jprodriguez22 <jp.rodriguez22@uniandes.edu.co>

--- vinilos_app/app/src/androidTest/java/com/example/vinilos/testListaAlbum.java
+++ vinilos_app/app/src/androidTest/java/com/example/vinilos/testListaAlbum.java
@@ -1,16 +1,12 @@
 package com.example.vinilos;
 
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static org.hamcrest.Matchers.allOf;
 
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.filters.LargeTest;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import com.example.vinilos.R;
 import com.example.vinilos.ui.MainActivity;
 
 import org.junit.Rule;
