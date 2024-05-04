6114ae1596be0aee856c09134024bb5770db06aa
Sebastian Calderon <js.calderonb12@uniandes.edu.co>
2024-05-03 21:17:49 -0500

    maquetado detalle banda (#97)
    Co-authored-by: imdiegodev1 <da.naranjo10@uniandes.edu.co>

--- vinilos_app/app/src/main/java/com/example/vinilos/ui/DetalleBanda.kt
+++ vinilos_app/app/src/main/java/com/example/vinilos/ui/DetalleBanda.kt
@@ -3,4 +3,10 @@ package com.example.vinilos.ui
 import androidx.fragment.app.Fragment
 
 class DetalleBanda : Fragment(){
+
+    companion object {
+        fun newInstance() = DetalleBanda()
+    }
+
+
 }
